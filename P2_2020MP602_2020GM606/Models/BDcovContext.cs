c60664ca498ec617d6e9ab115621c9c8f888d0a8
DavidM839 <107301911+DavidM839@users.noreply.github.com>
2023-04-29 13:10:01 -0600

    commit

--- P2_2020MP602_2020GM606/Models/BDcovContext.cs
+++ P2_2020MP602_2020GM606/Models/BDcovContext.cs
@@ -1,6 +1,13 @@
-﻿namespace P2_2020MP602_2020GM606.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace P2_2020MP602_2020GM606.Models
 {
-    public class BDcovContext
+    public class BDcovContext : DbContext
     {
+        public BDcovContext(DbContextOptions options) : base(options)
+        {
+        }
+       // public DbSet<equipo> equipo { get; set; }
+
     }
 }
