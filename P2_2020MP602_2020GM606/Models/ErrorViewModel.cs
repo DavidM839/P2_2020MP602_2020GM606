c508115ad7c03b483016079b8671119fe66cf66a
DavidM839 <107301911+DavidM839@users.noreply.github.com>
2023-04-29 12:24:14 -0600

    Agregar archivos de proyecto.

---
+++ P2_2020MP602_2020GM606/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P2_2020MP602_2020GM606.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
